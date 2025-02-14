3b3a96887cac8a3d96a5e18c552bbd3debb2e590
mayur-driod <settymayurk@gmail.com>
2025-02-14 10:35:47 +0530

    added name and contact

---
+++ new-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello! My name is Mayur</h1>
+      <p>My contact is <a>settymayurk@gmail.com</a></p>
+    </>
+  )
+}
+
+export default App
